f33fb21d190cbf1da7a0b132fc768442b68a881b
Alex Lopatin <lopatin.rnd@gmail.com>
2017-03-30 10:07:55 +0300

    deck without values

--- cards.rb
+++ cards.rb
@@ -1,9 +1,25 @@
 class Deck
 
-  attr_accessor :card
+  attr_accessor :card, :deck
 
-  @deck_sample = [(2..10).to_a, 'J', 'Q', 'K', 'A'].flatten
-  @siut = [
-           
+  @@deck_sample = [(2..10).to_a, 'J', 'Q', 'K', 'A'].flatten
+  p @@deck_sample
+  # 2661 - Heart, 2662 - Diamond, 2667 - Club, 2664 - Spade
+  @@siut = ["\u2661", "\u2662", "\u2667", "\u2664"]
+  p @@siut
+  
+  def initialize
+    @deck = @@deck_sample.map do |card|
+      @@siut.map { |siut| "#{card}" + "#{siut}" }
+    end
+    @deck.flatten!.shuffle!
+  end
+  
 
 end
+
+a = Deck.new
+puts a.deck
+
+# TODO: make deck as a hash { "card + siut" => value }
+# or array ['card siute', value]
