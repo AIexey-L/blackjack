accd3a6e9278d77bc10d56e8521ef91b36357168
Alexey <cosmospirat@gmail.com>
2017-04-06 16:17:37 +0300

    [ai.rb] added logic draft

--- ai.rb
+++ ai.rb
@@ -1,9 +1,17 @@
 module Ai
 
   def computer_turn
-    if app.send :score, @computer < 21
-      add_card(app.computer)
-      #TODO
+    begin
+      add_card(app.computer) if app.send :score, @computer < 21
+      puts "*** computer turn ***"
+      puts "*** adding card ***"
+      puts "*** open cards! ***"
+      app.open_status_computer
+      app.status_human
+    rescue RuntimeError
+      if app.send :score, @computer >= 17
+        #TODO
+      end
     end
   end
 end
